f2d1a46c1d6641fd1a2b760de22adca84a8f3ae9
Samuel Kyletoft <samuel@kyletoft.se>
2024-01-31 14:37:58 +0100

    custom-fe7: Add missing `const`s

--- inc/custom-fe7.h
+++ inc/custom-fe7.h
@@ -1,4 +1,4 @@
 #pragma once
 
 constexpr unsigned int custom_fe7_len = 2048;
-extern unsigned char custom_fe7_tilemap[custom_fe7_len];
+extern const unsigned char custom_fe7_tilemap[custom_fe7_len];
