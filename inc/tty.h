08907761addfd7f9500c9dc547254abc22de026d
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-02 02:14:30 +0100

    tty: Add (unused as of yet) data members for the internal buffer

--- inc/tty.h
+++ inc/tty.h
@@ -8,6 +8,11 @@ void decompress_1bpp_to_4bpp(void *dest, const u32 *__restrict src, size_t len);
 
 class TtyMode : public state::Mode {
   private:
+	static constexpr size_t LINE_LEN = 240 / 8;
+	static constexpr size_t SIZE = 240 * 160 / (8 * 8);
+	char buffer[SIZE];
+	size_t len = 0;
+
   public:
 	TtyMode()
 		: Mode() {}
