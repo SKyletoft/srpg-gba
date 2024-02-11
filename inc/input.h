20378afd33b87bfdebe419e599ff45a602eee442
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-11 23:55:34 +0100

    input: Init input types

---
+++ inc/input.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace input {
+
+enum class Button { A, B, L, R, Start, Select, Up, Down, Left, Right };
+enum class InputState { Up, Pressed, Held, Released };
+
+} // namespace input
---
+++ src/input.cpp
@@ -0,0 +1,9 @@
+#include "input.h"
+
+#include <array>
+
+namespace input {
+
+std::array<InputState, 10> BUTTON_STATES;
+
+} // namespace input
