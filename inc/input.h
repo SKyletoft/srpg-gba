bcb5e779ebe098cfd3797d22c2d6b4e575ce7d5a
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-12 11:05:29 +0100

    input: Add is_down and is_up to InputState (+ boilerplate to enable methods)

--- inc/input.h
+++ inc/input.h
@@ -3,6 +3,24 @@
 namespace input {
 
 enum class Button { A, B, L, R, Start, Select, Up, Down, Left, Right };
-enum class InputState { Up, Pressed, Held, Released };
+
+class InputState {
+  public:
+	enum InternalInputState { Up, Pressed, Held, Released };
+
+  private:
+	InternalInputState state = InputState::Up;
+
+  public:
+	bool is_down();
+	bool is_up();
+
+	bool operator==(InternalInputState const &rhs) const;
+	bool operator==(InputState const &rhs) const;
+
+	InputState(InternalInputState const &from)
+		: state(from) {}
+	InputState() {}
+};
 
 } // namespace input
--- src/input.cpp
+++ src/input.cpp
@@ -6,4 +6,19 @@ namespace input {
 
 std::array<InputState, 10> BUTTON_STATES;
 
+bool InputState::is_down() {
+	return this->state == InputState::Pressed
+		   || this->state == InputState::Held;
+}
+
+bool InputState::is_up() { return !this->is_down(); }
+
+bool InputState::operator==(InternalInputState const &rhs) const {
+	return this->state == rhs;
+}
+
+bool InputState::operator==(InputState const &rhs) const {
+	return this->state == rhs.state;
+}
+
 } // namespace input
