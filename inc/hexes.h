5df6725d1874fb7537771505410c40dca79aa574
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-13 15:52:02 +0100

    hexes: Protect CubeCoord invariant

--- inc/hexes.h
+++ inc/hexes.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <array>
 #include <cstddef>
+#include <optional>
 
 extern "C" {
 #include <tonc_types.h>
@@ -8,13 +10,28 @@ extern "C" {
 
 namespace hexes {
 
+struct CubeCoord;
+struct OffsetXYCoord;
+struct AxialCoord;
+
 enum class Direction { R, UR, UL, L, DL, DR };
 
 struct CubeCoord {
-	s16 q;
-	s16 r;
-	s16 s;
+	s16 const q;
+	s16 const r;
+	s16 const s;
 
+  private:
+	constexpr CubeCoord(s16 q, s16 r, s16 s)
+		: q(q)
+		, r(r)
+		, s(s) {}
+
+	constexpr static bool check_invariant(s16 q, s16 r, s16 s) {
+		return (q + r + s) == 0;
+	}
+
+  public:
 	CubeCoord add(CubeCoord) const;
 	CubeCoord operator+(CubeCoord) const;
 	CubeCoord subtract(CubeCoord) const;
@@ -25,6 +42,20 @@ struct CubeCoord {
 	s16 distance(CubeCoord) const;
 
 	constexpr CubeCoord neighbour(Direction dir) const;
+
+	static constexpr std::optional<CubeCoord> from_qrs(s16 q, s16 r, s16 s) {
+		if (check_invariant(q, r, s)) {
+			return CubeCoord(q, r, s);
+		}
+		return std::nullopt;
+	}
+
+	static constexpr CubeCoord unsafe_from_qrs_unchecked(s16 q, s16 r, s16 s) {
+		return CubeCoord(q, r, s);
+	}
+
+	friend CubeCoord offsetXY_to_cube(OffsetXYCoord);
+	friend CubeCoord axial_to_cube(AxialCoord);
 };
 
 struct OffsetXYCoord {
@@ -46,13 +77,13 @@ CubeCoord axial_to_cube(AxialCoord hex);
 AxialCoord cube_to_axial(CubeCoord hex);
 AxialCoord offsetXY_to_axial(OffsetXYCoord hex);
 
-static constexpr CubeCoord CUBE_DIRECTION_VECTORS[] = {
-	CubeCoord(1, 0, -1),
-	CubeCoord(1, -1, 0),
-	CubeCoord(0, -1, 1),
-	CubeCoord(-1, 0, 1),
-	CubeCoord(-1, 1, 0),
-	CubeCoord(0, 1, -1),
+static constexpr std::array<CubeCoord, 6> CUBE_DIRECTION_VECTORS = {
+	CubeCoord::unsafe_from_qrs_unchecked(1, 0, -1),
+	CubeCoord::unsafe_from_qrs_unchecked(1, -1, 0),
+	CubeCoord::unsafe_from_qrs_unchecked(0, -1, 1),
+	CubeCoord::unsafe_from_qrs_unchecked(-1, 0, 1),
+	CubeCoord::unsafe_from_qrs_unchecked(-1, 1, 0),
+	CubeCoord::unsafe_from_qrs_unchecked(0, 1, -1),
 };
 
 constexpr CubeCoord CubeCoord::neighbour(Direction dir) const {
