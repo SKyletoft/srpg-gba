496eb3302e835fa39894dcf6d125776636187875
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-14 00:29:01 +0100

    config: Add popup mode

--- src/config.cpp
+++ src/config.cpp
@@ -3,6 +3,7 @@
 
 #include "hexmap.h"
 #include "map.h"
+#include "popup.h"
 #include "scrolling_hexgrid.h"
 #include "scrolling_rainbow.h"
 #include "test_map.h"
@@ -21,11 +22,13 @@ scrolling_rainbow::ScrollingRainbow rainbow{};
 scrolling_hexgrid::ScrollingHexgrid hex{};
 hexmap::Hexmap hexmap{test_map::map};
 map::MapMode old_map{};
+popup::PopupMenu popup{};
 
-std::array<state::Mode *, 3> const modes_data{
+std::array<state::Mode *, 4> const modes_data{
 	&hexmap,
 	&old_map,
 	&debug::tty_mode,
+	&popup,
 };
 std::span<state::Mode *const> modes{modes_data};
 
