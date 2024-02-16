2ef26e5de6b343f785918905b2f86c9fe2477512
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-16 02:30:50 +0100

    cursor_scroller: Start handling input

--- inc/cursor_scroller.h
+++ inc/cursor_scroller.h
@@ -13,11 +13,16 @@ namespace cursor_scroller {
 
 using hexes::CubeCoord;
 
+static constexpr s16 COOLDOWN = 8;
+
 class CursorScroller : public scrolling_map::ScrollingMap {
   protected:
 	CubeCoord cursor{};
+	/// Up, Down, Left, Right
+	std::array<s16, 4> directional_cooldowns{0, 0, 0, 0};
 
 	void update() override;
+	void handle_input();
 
   public:
 	CursorScroller(s16 width, s16 height)
--- src/cursor_scroller.cpp
+++ src/cursor_scroller.cpp
@@ -1,10 +1,44 @@
 #include "cursor_scroller.h"
 
+#include "hexes.h"
+#include "input.h"
+#include <array>
+#include <tuple>
+
 namespace cursor_scroller {
 
+using hexes::Direction;
+using input::Button;
+
 void CursorScroller::update() {
+	this->handle_input();
 	this->update_layer(this->layer0);
 	this->update_layer(this->layer1);
+}
+
+void CursorScroller::handle_input() {
+	std::array<std::tuple<Button, size_t, Direction>, 4> const inputs{
+		std::tuple
+		// Thanks C++ not needing nested {} for 2d arrays so this breaks instead
+		{Button::Up, 0, Direction::UL},
+		{Button::Down, 1, Direction::DL},
+		{Button::Left, 2, Direction::L},
+		{Button::Right, 3, Direction::R},
+	};
+	for (auto const &[button, index, dir] : inputs) {
+		if (input::get_button(button).is_down()
+			&& this->directional_cooldowns[index] >= 0)
+		{
+			this->cursor = this->cursor + dir;
+			this->directional_cooldowns[index] = COOLDOWN;
+		}
+	}
+
+	if (input::get_button(Button::R).is_down()
+		&& input::get_button(Button::L).is_down())
+	{
+		state::next_state = 1;
+	}
 }
 
 } // namespace cursor_scroller
