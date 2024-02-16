cb08cfac77c723203d367f39ef4db0f6587ede50
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-16 19:52:35 +0100

    cursor_scroller: Use 16x16 sprite template

--- inc/cursor_scroller.h
+++ inc/cursor_scroller.h
@@ -19,7 +19,7 @@ static constexpr s16 COOLDOWN = 8;
 class CursorScroller : public scrolling_map::ScrollingMap {
   protected:
 	CubeCoord cursor = CubeCoord::from_offset_xy({.col = 5, .row = 5});
-	sprite::Sprite cursor_sprite{};
+	sprite::Sprite cursor_sprite = sprite::X16;
 	/// Up, Down, Left, Right
 	std::array<s16, 4> directional_cooldowns{0, 0, 0, 0};
 
