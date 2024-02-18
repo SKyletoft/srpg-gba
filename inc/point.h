ffebb64f00a9f3a9fb3db560270fc2e77891398f
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-18 21:15:30 +0100

    point: Alias Self for line length
    SQUASHME: Alias Point<T> to Self even more

--- inc/point.h
+++ inc/point.h
@@ -8,53 +8,58 @@ template <typename T>
 concept NumericType = std::integral<T> || std::floating_point<T>;
 
 template <NumericType T> struct Point {
+	using Self = Point<T>;
+
 	T x{0};
 	T y{0};
 
-	[[nodiscard]] constexpr Point<T> operator+(Point<T> const &rhs) const noexcept {
+	[[nodiscard]] constexpr Self operator+(Point<T> const &rhs) const noexcept {
 		return Point{
+
+	[[nodiscard]] constexpr Self operator+(Self const &rhs) const noexcept {
+		return Self{
 			.x = (T)(this->x + rhs.x),
 			.y = (T)(this->y + rhs.y),
 		};
 	}
 
-	[[nodiscard]] constexpr Point<T> operator-(Point<T> const &rhs) const noexcept {
-		return Point{
+	[[nodiscard]] constexpr Self operator-(Self const &rhs) const noexcept {
+		return Self{
 			.x = (T)(this->x - rhs.x),
 			.y = (T)(this->y - rhs.y),
 		};
 	}
 
-	[[nodiscard]] constexpr Point<T> operator*(T const &rhs) const noexcept {
-		return Point{
+	[[nodiscard]] constexpr Self operator*(T const &rhs) const noexcept {
+		return Self{
 			.x = (T)(this->x * rhs),
 			.y = (T)(this->y * rhs),
 		};
 	}
 
-	[[nodiscard]] constexpr Point<T> operator/(T const &rhs) const noexcept {
-		return Point{
+	[[nodiscard]] constexpr Self operator/(T const &rhs) const noexcept {
+		return Self{
 			.x = (T)(this->x / rhs),
 			.y = (T)(this->y / rhs),
 		};
 	}
 
-	constexpr Point<T> &operator+=(Point<T> const &rhs) noexcept {
+	constexpr Self &operator+=(Self const &rhs) noexcept {
 		*this = *this + rhs;
 		return *this;
 	}
 
-	constexpr Point<T> &operator-=(Point<T> const &rhs) noexcept {
+	constexpr Self &operator-=(Self const &rhs) noexcept {
 		*this = *this - rhs;
 		return *this;
 	}
 
-	constexpr Point<T> &operator*=(T const &rhs) noexcept {
+	constexpr Self &operator*=(T const &rhs) noexcept {
 		*this = *this * rhs;
 		return *this;
 	}
 
-	constexpr Point<T> &operator/=(T const &rhs) noexcept {
+	constexpr Self &operator/=(T const &rhs) noexcept {
 		*this = *this / rhs;
 		return *this;
 	}
