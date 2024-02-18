e97123fd0466ccfaf3814465a494fab8f5db7039
Ghaith Abou Dan <g.a.1997@hotmail.com>
2024-02-18 21:41:18 +0100

    audio: Add some initial audio functionality in main, restores still broken

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,18 @@
 #include "state.h"
 #include "tty.h"
 #include "util.h"
-
+#include <maxmod.h>
+#include "soundbank.h"
+#include "soundbank_bin.h"
 #include <span>
+
+
+extern "C" {
+	#include <tonc_bios.h>
+	#include <tonc_memdef.h>
+	#include <tonc_irq.h>
+	#include <tonc.h>
+}
 
 namespace config {
 extern std::span<state::Mode *const> modes;
@@ -23,6 +33,13 @@ extern "C" [[noreturn]] void _exit(int) {
 }
 
 int main() {
+
+	irq_init(nullptr);
+	irq_add(II_VBLANK, mmVBlank);
+	irq_enable(II_VBLANK);
+
+	mmInitDefault((mm_addr) soundbank_bin, 8);
+	mmStart(MOD_BAD_APPLE, MM_PLAY_LOOP);
 	state::next_state = 0;
 
 	state::current_state = state::next_state;
@@ -31,9 +48,15 @@ int main() {
 	config::modes[state::current_state]->restore();
 
 	for (;;) {
+
+		mmFrame();
+		//util::wait_for_vsync();
+		VBlankIntrWait();
 		util::wait_for_drawing_start();
 		if (state::current_state != state::next_state) {
-			util::wait_for_vsync();
+
+			VBlankIntrWait();
+			//util::wait_for_vsync();
 			if (config::modes[state::current_state]->blackout()
 				|| config::modes[state::next_state]->blackout())
 			{
@@ -42,18 +65,19 @@ int main() {
 			config::modes[state::current_state]->suspend();
 			state::last_state = state::current_state;
 			state::current_state = state::next_state;
-			util::wait_for_vsync();
+			VBlankIntrWait();
+			//util::wait_for_vsync();
 			config::modes[state::current_state]->restore();
 		}
 
 		input::poll();
 		config::modes[state::current_state]->update();
-
 		for (auto mode : config::modes) {
 			mode->always_update();
 		}
 
 		perf::record_frame();
+		mmFrame();
 		util::wait_for_drawing_complete();
 		config::modes[state::current_state]->vsync_hook();
 
