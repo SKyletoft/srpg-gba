01a8d02a52c9afc61a03b39e77717c267fa2e5f3
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-23 03:15:40 +0100

    context_menu: Alias std::ranges::views

--- demo/src/context_menu.cpp
+++ demo/src/context_menu.cpp
@@ -4,10 +4,12 @@
 
 namespace context_menu {
 
+namespace rv = std::ranges::views;
+
 bool ContextMenu::blackout() { return false; }
 
 void ContextMenu::restore() {
-	for (auto const idx : std::ranges::views::iota(0uz, 128uz)) {
+	for (auto const idx : rv::iota(0uz, 128uz)) {
 		sprite::HardwareSprite::hide(idx);
 	}
 	this->PopupMenu::restore();
