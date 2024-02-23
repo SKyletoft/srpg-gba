ad079f2ce7fa7380d13dd5aa73d9a7fbf602ecb4
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-23 01:52:50 +0100

    config: Add header version

---
+++ demo/inc/config.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "debug.h"
+#include "export.h"
+#include "sprite.h"
+#include "tiles.h"
+#include "tty.h"
+#include <array>
+
+#include "context_menu.h"
+#include "map.h"
+#include "soundbank.h"
+#include "test_map.h"
+#include "unit.h"
+
+namespace config {
+
+using tiles::BG_PALETTE_MEMORY;
+using tiles::SPRITE_PALETTE_MEMORY;
+using unit::Stats;
+using unit::Unit;
+
+extern std::array<Unit, 12> user_army;
+extern size_t user_soldier_count;
+extern map::Map map;
+extern context_menu::ContextMenu popup;
+extern std::array<state::Mode *, 3> const modes_data;
+extern u32 startup_song;
+
+} // namespace config
