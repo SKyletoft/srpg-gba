56889e084fd70bf6c26663001f0b9409b008a537
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-26 16:39:34 +0100

    move_unit: Implement fast palette override function

--- demo/inc/move_unit.h
+++ demo/inc/move_unit.h
@@ -2,7 +2,18 @@
 
 #include "map.h"
 
+#include <span>
+#include <vector>
+
+extern "C" {
+#include <tonc_types.h>
+}
+
 namespace move_unit {
+
+void update_palettes_of(
+	std::span<hexes::CubeCoord const> highlights, u8 new_palette
+);
 
 class MoveUnit : public map::Map {
   public:
--- demo/src/move_unit.cpp
+++ demo/src/move_unit.cpp
@@ -10,6 +10,35 @@ namespace move_unit {
 using input::Button;
 using input::InputState;
 
+void update_palettes_of(
+	std::span<hexes::CubeCoord const> highlights, u8 new_palette
+) {
+	for (auto const &tile : highlights) {
+		auto tile_coord =
+			(tile.to_pixel_space() - config::hexmap.layer0.pos.into<s32>()) / 8;
+		constexpr std::array<size_t, 12> tiles_offsets_in_hex = {
+			0, 1, 2, 32, 33, 34, 64, 65, 66, 96, 97, 98
+		};
+
+		tiles::ScreenEntry volatile *base;
+		if (!tile.is_odd()) {
+			base = &tiles::SCREENBLOCKS[config::hexmap.layer0.tile_map]
+									   [tile_coord.y * 32 + tile_coord.x];
+		} else {
+			tile_coord.x += 1;
+			base = &tiles::SCREENBLOCKS[config::hexmap.layer1.tile_map]
+									   [tile_coord.y * 32 + tile_coord.x];
+		}
+
+		for (size_t offset : tiles_offsets_in_hex) {
+			tiles::ScreenEntry volatile &se = base[offset];
+			tiles::ScreenEntry copy = se;
+			copy.palette = new_palette & 0b1111;
+			se = copy;
+		}
+	}
+}
+
 void MoveUnit::update() {
 	this->Map::update();
 
