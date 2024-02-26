bf3760e3e351ec9a02b9e0a36b938d2d5d28a678
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-26 16:41:37 +0100

    unit: Attempt at optimising accessible_tiles

--- demo/src/unit.cpp
+++ demo/src/unit.cpp
@@ -1,9 +1,17 @@
 #include "unit.h"
 
+#include "debug.h"
 #include "hexes.h"
 #include "sprite.h"
+#include "util.h"
 
 #include <algorithm>
+#include <queue>
+#include <unordered_set>
+
+extern "C" {
+#include <tonc_memmap.h>
+}
 
 namespace unit {
 
@@ -20,29 +28,74 @@ void Unit::render(Point<s16> camera_offset, u8 animation_cycle) const {
 	sprite.render(camera_offset);
 }
 
-	std::vector<CubeCoord> vec{};
-	// Todo: Replace this with a min-heap
-	std::vector<std::pair<CubeCoord, u8>> queue{{this->sprite.pos, 0}};
 std::vector<CubeCoord> Unit::accessible_tiles(Span2d<const u8> const &map
 ) const {
+	using CC_Depth = std::pair<CubeCoord, u8>;
+	struct CompareDepth {
+		bool operator()(const CC_Depth &a, const CC_Depth &b) const {
+			return a.second > b.second;
+		}
+	};
+	auto const hex_to_idx = [&](CubeCoord cc) {
+		auto const xy = cc.to_offset_xy();
+		return (size_t)xy.col * map.width + (size_t)xy.row;
+	};
+
+	// Should be π(mov + 0.5)², but close enough
+	// size_t const max_len =
+	//	(size_t)((this->stats.movement + 1) * (this->stats.movement + 1) * 3);
+	static std::vector<CubeCoord> vec{};
+	vec.clear();
+
+	static std::priority_queue<CC_Depth, std::vector<CC_Depth>, CompareDepth>
+		queue{};
+	while (!queue.empty()) {
+		queue.pop();
+	}
+
+	// std::vector<CC_Depth> queue{};
+	// queue.reserve(max_len);
+	static std::vector<bool> visited(map.width * map.height, false);
+	for (size_t i = 0; i < visited.size(); ++i) {
+		visited[i] = false;
+	}
+
+	queue.push({this->sprite.pos, 0});
+	// queue.push_back({this->sprite.pos, 0});
+	visited[hex_to_idx(this->sprite.pos)] = true;
 
 	while (!queue.empty()) {
-		auto const [curr, depth] = queue.back();
-		queue.pop_back();
+		// auto [curr, depth] = *queue.begin();
+		// queue.erase(queue.begin());
+		auto [curr, depth] = queue.top();
+		queue.pop();
+
+		if (depth > this->stats.movement) {
+			break;
+		}
+
 		vec.push_back(curr);
 
-		if (depth >= this->stats.movement) {
-			continue;
-		}
-		for (auto neighbour : hexes::CUBE_DIRECTION_VECTORS) {
-			neighbour += curr;
-			if (r::find_if(queue, [&](auto c) { return c.first == neighbour; })
-				== queue.end())
+		for (auto const &neighbour : hexes::CUBE_DIRECTION_VECTORS) {
+			auto neighbour_ = neighbour + curr;
+			auto xy = neighbour_.to_offset_xy();
+			if (xy.col < 0 || map.width <= xy.col || xy.row < 0
+				|| map.height <= xy.row)
 			{
-				queue.push_back({neighbour, depth + 1});
+				continue;
+			}
+
+			size_t idx = hex_to_idx(neighbour_);
+			if (!visited[idx]) {
+				// queue.push_back({neighbour_, depth + 1});
+				queue.push({neighbour_, depth + 1});
+				visited[idx] = true;
 			}
 		}
 	}
+
+	debug::print("Checked tiles: ");
+	debug::println((int)visited.size());
 
 	return vec;
 }
