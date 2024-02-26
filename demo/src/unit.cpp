7767f5054afda1814783895256876f43b840fd22
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-26 03:16:33 +0100

    unit: Implement initial iffy version of accessible tiles

--- demo/src/unit.cpp
+++ demo/src/unit.cpp
@@ -1,8 +1,13 @@
 #include "unit.h"
+
 #include "hexes.h"
 #include "sprite.h"
 
+#include <algorithm>
+
 namespace unit {
+
+namespace r = std::ranges;
 
 using hexes::CubeCoord;
 using mdspan::Span2d;
@@ -16,7 +21,28 @@ void Unit::render(Point<s16> camera_offset, u8 animation_cycle) const {
 }
 
 std::vector<CubeCoord> Unit::accessible_tiles(Span2d<u8> const &map) const {
-	std::vector vec{this->sprite.pos};
+	std::vector<CubeCoord> vec{};
+	// Todo: Replace this with a min-heap
+	std::vector<std::pair<CubeCoord, u8>> queue{{this->sprite.pos, 0}};
+
+	while (!queue.empty()) {
+		auto const [curr, depth] = queue.back();
+		queue.pop_back();
+		vec.push_back(curr);
+
+		if (depth >= this->stats.movement) {
+			continue;
+		}
+		for (auto neighbour : hexes::CUBE_DIRECTION_VECTORS) {
+			neighbour += curr;
+			if (r::find_if(queue, [&](auto c) { return c.first == neighbour; })
+				== queue.end())
+			{
+				queue.push_back({neighbour, depth + 1});
+			}
+		}
+	}
+
 	return vec;
 }
 
