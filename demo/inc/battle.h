9bc76a545d3d8f6bf2f9d634e5756a726fd3c4c3
Samuel Kyletoft <samuel@kyletoft.se>
2024-02-28 16:39:27 +0100

    battle: Stable state with two animated characters

--- demo/inc/battle.h
+++ demo/inc/battle.h
@@ -1,11 +1,31 @@
 #pragma once
 
+#include "sprite.h"
 #include "state.h"
 
 namespace battle {
 
+using sprite::HardwareSprite;
+
 class Battle : public state::Mode {
 	u8 frame = 0;
+	u32 time = 0;
+
+	HardwareSprite left{
+		.object_mode = sprite::ObjectMode::Normal,
+		.x = 20,
+		.size = 0b011,
+		.tile_index = 1,
+		.palette = 0,
+	};
+	HardwareSprite right{
+		.object_mode = sprite::ObjectMode::Normal,
+		.x = 150,
+		.horizontal_flip = true,
+		.size = 0b011,
+		.tile_index = 1,
+		.palette = 1,
+	};
 
   public:
 	void update() override;
--- demo/src/battle.cpp
+++ demo/src/battle.cpp
@@ -12,33 +12,45 @@ extern "C" {
 }
 
 namespace battle {
+using tiles::Colour;
 
-void Battle::update() {}
+void Battle::update() {
+	this->time++;
+	if (this->time > 5) {
+		this->time = 0;
+		this->frame = (u8)((this->frame + 1) % 5);
+	}
+}
 
 void Battle::restore() {
 	std::memcpy(
-		&tiles::SPRITE_CHARBLOCK[0][1], battle_aniTiles, sizeof(battle_aniTiles)
+		tiles::SPRITE_CHARBLOCK[0], battle_aniTiles, sizeof(battle_aniTiles)
 	);
-	// tiles::SPRITE_PALETTE_MEMORY[0] = tiles::Palette{{
-	//	tiles::TRANSPARENT,
-	//	tiles::BLACK,
-	//	tiles::Colour(31, 14, 14),
-	//	tiles::Colour(31, 25, 25),
-	// }};
-	tiles::SPRITE_PALETTE_MEMORY[0] = *(tiles::Palette *)battle_aniPal;
+	tiles::SPRITE_PALETTE_MEMORY[0] = tiles::Palette{{
+		tiles::TRANSPARENT,
+		Colour(15, 15, 15),
+		Colour(15, 0, 0),
+		Colour(31, 0, 0),
+		Colour(25, 25, 25),
+	}};
+	tiles::SPRITE_PALETTE_MEMORY[1] = tiles::Palette{{
+		tiles::TRANSPARENT,
+		Colour(15, 15, 15),
+		Colour(0, 0, 15),
+		Colour(0, 0, 31),
+		Colour(25, 25, 25),
+	}};
 
 	// REG_DISPCNT = DCNT_MODE0 | DCNT_OBJ | DCNT_OBJ_1D;
 	REG_DISPCNT = DCNT_MODE0 | DCNT_BG0 | DCNT_BG1 | DCNT_OBJ | DCNT_OBJ_1D;
-
-	auto sprite = sprite::HardwareSprite{
-		.object_mode = sprite::ObjectMode::Normal,
-		.tile_index = 1,
-		.palette = 0,
-	};
-	sprite.set_size(sprite::SpriteSize::x64);
-	sprite.write_to_screen(1);
 }
 
-void Battle::vsync_hook() {}
+void Battle::vsync_hook() {
+	this->left.tile_index = this->frame * 64;
+	this->left.write_to_screen(1);
+
+	this->right.tile_index = this->frame * 64;
+	this->right.write_to_screen(2);
+}
 
 } // namespace battle
