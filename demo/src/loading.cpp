3cde1ffab21d51557df49a087395b3278622e5e2
Samuel Kyletoft <samuel@kyletoft.se>
2024-03-02 18:44:18 +0100

    loading: Add used red palette

--- demo/src/loading.cpp
+++ demo/src/loading.cpp
@@ -46,6 +46,14 @@ constexpr tiles::Palette RED_ACTIVE{
 	Colour(5, 0, 0),
 };
 
+constexpr tiles::Palette RED_USED{
+	tiles::TRANSPARENT,
+	Colour::from_24bit_colour(0x68, 0x1C, 0x28),
+	Colour::from_24bit_colour(0x30, 0x30, 0x30),
+	Colour(16, 0, 0),
+	Colour(2, 0, 0),
+};
+
 void load_map_graphics() {
 	for (auto i : rv::iota(0uz, 128uz)) {
 		sprite::HardwareSprite::hide(i);
@@ -79,6 +87,7 @@ void load_map_graphics() {
 	tiles::SPRITE_PALETTE_MEMORY[1] = BLUE_ACTIVE;
 	tiles::SPRITE_PALETTE_MEMORY[2] = RED_ACTIVE;
 	tiles::SPRITE_PALETTE_MEMORY[3] = BLUE_USED;
+	tiles::SPRITE_PALETTE_MEMORY[4] = RED_USED;
 
 	REG_BG0CNT = (u16)(BG_CBB(config::hexmap.layer0.tile_source)
 					   | BG_SBB(config::hexmap.layer0.tile_map) | BG_4BPP
