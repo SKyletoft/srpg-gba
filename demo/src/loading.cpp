6b2d4d5c9f7ae0ccf2b8e362fcb3276f0aeb63c7
Samuel Kyletoft <samuel@kyletoft.se>
2024-03-03 00:46:03 +0100

    loading: Move Palettes to header

--- demo/src/loading.cpp
+++ demo/src/loading.cpp
@@ -20,40 +20,6 @@ namespace loading {
 
 namespace rv = std::ranges::views;
 
-using tiles::Colour;
-
-constexpr tiles::Palette BLUE_ACTIVE{
-	tiles::TRANSPARENT,
-	Colour::from_24bit_colour(0x1C, 0x71, 0xD8),
-	Colour::from_24bit_colour(0x30, 0x30, 0x30),
-	Colour::from_24bit_colour(0x00, 0xBD, 0xEA),
-	Colour::from_24bit_colour(0x18, 0x34, 0x97),
-};
-
-constexpr tiles::Palette BLUE_USED{
-	tiles::TRANSPARENT,
-	Colour::from_24bit_colour(0x66, 0x66, 0xC8),
-	Colour::from_24bit_colour(0x30, 0x30, 0x30),
-	Colour::from_24bit_colour(0x7D, 0x7D, 0xDA),
-	Colour::from_24bit_colour(0x3B, 0x3B, 0x87),
-};
-
-constexpr tiles::Palette RED_ACTIVE{
-	tiles::TRANSPARENT,
-	Colour::from_24bit_colour(0xC0, 0x1C, 0x28),
-	Colour::from_24bit_colour(0x30, 0x30, 0x30),
-	Colour(31, 0, 0),
-	Colour(5, 0, 0),
-};
-
-constexpr tiles::Palette RED_USED{
-	tiles::TRANSPARENT,
-	Colour::from_24bit_colour(0x68, 0x1C, 0x28),
-	Colour::from_24bit_colour(0x30, 0x30, 0x30),
-	Colour(16, 0, 0),
-	Colour(2, 0, 0),
-};
-
 void load_map_graphics() {
 	for (auto i : rv::iota(0uz, 128uz)) {
 		sprite::HardwareSprite::hide(i);
