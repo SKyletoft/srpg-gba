37e71841ac002b0457274676f5b90108f4bbfa49
Samuel Kyletoft <samuel@kyletoft.se>
2024-03-04 21:41:57 +0100

    battle: Add damage number sprites

--- demo/src/battle.cpp
+++ demo/src/battle.cpp
@@ -2,16 +2,19 @@
 
 #include "audio.h"
 #include "config.h"
+#include "font.h"
 #include "sprite.h"
 #include "state.h"
 #include "tiles.h"
+#include "tty.h"
 #include "util.h"
-#include "config.h"
 
 #include <array>
 #include <cstring>
+#include <format>
 #include <functional>
 #include <ranges>
+#include <tonc_tte.h>
 #include <tuple>
 
 extern "C" {
@@ -30,8 +33,13 @@ namespace battle {
 
 namespace rv = std::ranges::views;
 
+using hexmap::STile;
 using tiles::Colour;
 using tiles::Palette;
+using tiles::SPRITE_CHARBLOCK;
+
+STile *const post_animation_tiles = &SPRITE_CHARBLOCK[0][5 * 64];
+STile *const alphabet_tiles = post_animation_tiles + 8;
 
 void Battle::animation_update() {
 	this->time++;
@@ -63,18 +71,23 @@ void Battle::animation_update() {
 
 void Battle::fight() {
 	start_battle_bgm();
-	std::function attack = [&](Unit &attacker, Unit &defender) {
-		s8 damage =
-			(s8)std::max(0, attacker.stats.attack - defender.stats.defence);
+	std::function attack = [&](Unit &attacker, Unit &defender, STile *tile) {
+		s8 damage = s8(
+			std::clamp(attacker.stats.attack - defender.stats.defence, 0, 9999)
+		);
+		std::string damage_text = std::format("{:4}", damage);
+		for (size_t i = 0; i < 4; ++i) {
+			tile[i] = alphabet_tiles[damage_text[i] - ' '];
+		}
 		defender.stats.health -= damage;
 	};
 
-	attack(*this->left_unit, *this->right_unit);
+	attack(*this->left_unit, *this->right_unit, post_animation_tiles);
 	this->continue_to_second_round = this->right_unit->stats.health > 0;
 	if (!this->continue_to_second_round) {
 		return;
 	}
-	attack(*this->right_unit, *this->left_unit);
+	attack(*this->right_unit, *this->left_unit, post_animation_tiles + 4);
 }
 
 void Battle::update() {
@@ -100,6 +113,7 @@ void Battle::restore() {
 	std::memcpy(
 		tiles::SPRITE_CHARBLOCK[0], battle_aniTiles, sizeof(battle_aniTiles)
 	);
+	tty::decompress_1bpp_to_4bpp(alphabet_tiles, sys8Glyphs, '9' - ' ');
 
 	constexpr Palette RED{
 		tiles::TRANSPARENT,
@@ -138,14 +152,38 @@ void Battle::restore() {
 		HardwareSprite::hide(i);
 	}
 
+	HardwareSprite{
+		.y = 30,
+		.object_mode = sprite::ObjectMode::Normal,
+		.x = 150,
+		.horizontal_flip = false,
+		.tile_index = 5 * 64,
+		.palette = 0,
+	}
+		.set_size(sprite::SpriteSize::w32h8)
+		.write_to_screen(3);
+	HardwareSprite{
+		.y = 30,
+		.object_mode = sprite::ObjectMode::Normal,
+		.x = 120,
+		.horizontal_flip = false,
+		.tile_index = 5 * 64 + 4,
+		.palette = 0,
+	}
+		.set_size(sprite::SpriteSize::w32h8)
+		.write_to_screen(4);
+
 	REG_DISPCNT = DCNT_MODE0 | DCNT_BG0 | DCNT_BG1 | DCNT_OBJ | DCNT_OBJ_1D;
 
 	this->fight();
 }
 
 void Battle::suspend() {
-	this->left.hide(1);
-	this->right.hide(2);
+	HardwareSprite::hide(1);
+	HardwareSprite::hide(2);
+	HardwareSprite::hide(3);
+	HardwareSprite::hide(4);
+
 	stop_battle_bgm();
 
 	auto const maybe_kill = [](Unit *unit) {
