d6ef638e52ef3d18f3296d0a89c482638060d5c9
Samuel Kyletoft <samuel@kyletoft.se>
2024-03-04 04:03:25 +0100

    test_map/map1/map2: Three levels using the new format

--- inc/map2.h
+++ inc/map2.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include "level.h"
+#include "sprite.h"
+#include "unit.h"
+
 #include <array>
 
 namespace map2 {
+
+using sprite::HexSprite;
+using unit::Stats;
+using unit::Unit;
 
 // clang-format off
 static constexpr std::array<std::array<unsigned char,  20>,  9> map {
@@ -18,5 +26,64 @@ static constexpr std::array<std::array<unsigned char,  20>,  9> map {
 	{ 0, 1, 1, 1, 1, 8,15,15, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
 // clang-format on
+
+constexpr Unit enemies[]{
+	Unit{
+		.name = "Dude #1",
+		.portrait = 2,
+		.sprite =
+			HexSprite{
+				.pos = hexes::OffsetXYCoord(6, 5).to_cube_coord(),
+				.centre = {5, 0},
+				.size = sprite::SpriteSize::x16,
+				.hardware_id = 9,
+				.tile_index = 33,
+				.prio = 1,
+				.palette = 2,
+			},
+		.stats =
+			Stats{
+				.health = 18,
+				.max_health = 18,
+				.attack = 10,
+				.defence = 2,
+				.magic = 3,
+				.resistance = 3,
+				.speed = 9,
+				.luck = 2,
+				.movement = 3,
+			},
+		.animation_frames = 3,
+	},
+	Unit{
+		.name = "Dude #2",
+		.portrait = 3,
+		.sprite =
+			HexSprite{
+				.pos = hexes::OffsetXYCoord(7, 3).to_cube_coord(),
+				.centre = {5, 0},
+				.size = sprite::SpriteSize::x16,
+				.hardware_id = 3,
+				.tile_index = 33,
+				.prio = 1,
+				.palette = 2,
+			},
+		.stats =
+			Stats{
+				.health = 16,
+				.max_health = 16,
+				.attack = 5,
+				.defence = 4,
+				.magic = 3,
+				.resistance = 3,
+				.speed = 9,
+				.luck = 2,
+				.movement = 3,
+			},
+		.animation_frames = 3,
+	}
+};
+
+DECLARE_LEVEL(Level, map2::map, map2::enemies)
 
 } // namespace map2
