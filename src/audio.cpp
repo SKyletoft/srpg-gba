09320c988a19ceae5184d6dbeed89e6eec39a0f1
Ghaith Abou Dan <g.a.1997@hotmail.com>
2024-03-04 07:04:02 +0100

    audio: Formatting

--- src/audio.cpp
+++ src/audio.cpp
@@ -1,7 +1,7 @@
 #include "audio.h"
 
-#include "soundbank_bin.h"
 #include "soundbank.h"
+#include "soundbank_bin.h"
 #include <maxmod.h>
 
 extern "C" {
@@ -17,30 +17,17 @@ void initialise() {
 
 void play_song(u32 song) { mmStart(song, MM_PLAY_LOOP); }
 
-void play_sfx(u32 sfx) {
-	mmEffect(sfx);
-}
+void play_sfx(u32 sfx) { mmEffect(sfx); }
 
-void play_jingle(u32 jingle) {
-	mmJingle(jingle);
-}
+void play_jingle(u32 jingle) { mmJingle(jingle); }
 
-void set_bgm_volume(u32 volume){
-	mmSetModuleVolume(volume);
-}
+void set_bgm_volume(u32 volume) { mmSetModuleVolume(volume); }
 
-void set_jingle_volume(u32 volume){
-	mmSetJingleVolume(volume);
-}
+void set_jingle_volume(u32 volume) { mmSetJingleVolume(volume); }
 
-void pause_bgm(){
-	mmPause();
-}
-void resume_bgm(){
-	mmResume();
-}
+void pause_bgm() { mmPause(); }
 
-void stop_jingle(){
-	play_jingle(0);
-}
+void resume_bgm() { mmResume(); }
+
+void stop_jingle() { play_jingle(0); }
 } // namespace audio
