449ef079891064fd2ced14aa294283b9a4ede74a
Samuel Kyletoft <samuel@kyletoft.se>
2024-03-04 04:00:03 +0100

    level: Create level interface

---
+++ demo/inc/level.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "hl-map.h"
+#include "mdspan.h"
+#include "unit.h"
+
+#include "config.h"
+
+#include <ranges>
+#include <span>
+
+extern "C" {
+#include <tonc_types.h>
+}
+
+namespace level {
+
+namespace rv = std::ranges::views;
+
+class Level {
+  public:
+	constexpr Level() {}
+
+	constexpr virtual mdspan::Span2d<const u8> map() = 0;
+	constexpr virtual std::span<const unit::Unit> enemies() = 0;
+
+	constexpr virtual ~Level() {}
+};
+
+#define DECLARE_LEVEL(name, map_data, enemy_data)                              \
+	class name : public level::Level {                                         \
+		constexpr mdspan::Span2d<const unsigned char> map() override {                    \
+			return mdspan::Span2d<const unsigned char>(map_data);                         \
+		}                                                                      \
+		constexpr std::span<const unit::Unit> enemies() override { \
+			return std::span(enemy_data, sizeof(enemy_data) / sizeof(unit::Unit)); \
+		}                                                                      \
+	};
+
+void load_level(Level &level);
+
+}; // namespace level
