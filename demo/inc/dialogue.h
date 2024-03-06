21963037706d398b1a7e97256fbe2a0de20f8f83
Samuel Kyletoft <samuel@kyletoft.se>
2024-03-06 04:02:53 +0100

    dialogue: Dialogue data

--- demo/inc/dialogue.h
+++ demo/inc/dialogue.h
@@ -15,6 +15,40 @@ constexpr u16 TILE_SOURCE_3 = 1;
 constexpr u16 TILE_MAP_3 = 4;
 constexpr size_t LEFT_PAL = 0;
 constexpr size_t RIGHT_PAL = 3;
+
+struct LoadCharacter {
+	bool is_left;
+	size_t portrait;
+};
+
+struct Text {
+	std::string_view text;
+	bool highlight_left;
+};
+
+struct FocusCursor {
+	hexes::CubeCoord hex;
+};
+
+using TextBoxEvent = std::variant<Text, FocusCursor, LoadCharacter>;
+
+constexpr std::array<TextBoxEvent, 10> TEXT{
+	LoadCharacter{true, 1},
+	LoadCharacter{false, 0},
+	Text{
+		"Oh woe is me!\nMy life is an infinite dark-ness and all happiness "
+		"has\ncome to an end.",
+		true
+	},
+	Text{"If only we weren't\nsurrounded by the enemy!", true},
+	Text{"Look over there!\nThe leader!", false},
+	FocusCursor{hexes::CubeCoord::from_offset_xy({5, 15})},
+	LoadCharacter{true, 3},
+	Text{"Gwaahahaha!", true},
+	LoadCharacter{false, 1},
+	Text{"Time to fight, I guess!", false},
+};
+
 class Dialogue : public state::Mode {
   public:
 	bool blackout() override;
