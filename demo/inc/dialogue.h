fee881b25d74ed9f6c50ee3519d36f791078424f
Samuel Kyletoft <samuel@kyletoft.se>
2024-03-06 04:02:16 +0100

    dialogue: tile source, map and palette index constants

--- demo/inc/dialogue.h
+++ demo/inc/dialogue.h
@@ -9,6 +9,12 @@
 
 namespace dialogue {
 
+constexpr u16 TILE_SOURCE_2 = 2;
+constexpr u16 TILE_MAP_2 = 3;
+constexpr u16 TILE_SOURCE_3 = 1;
+constexpr u16 TILE_MAP_3 = 4;
+constexpr size_t LEFT_PAL = 0;
+constexpr size_t RIGHT_PAL = 3;
 class Dialogue : public state::Mode {
   public:
 	bool blackout() override;
